0dadd35517f8853b1a8005cf53f1c9805ec6b750
MarkCshandreOngkit <markongkit.001@gmail.com>
2025-03-11 18:39:27 +0800

    function asking number

--- batch_1/check_number_of_odds/check_number_of_odds.py
+++ batch_1/check_number_of_odds/check_number_of_odds.py
@@ -1,4 +1,8 @@
 #function that ask number
+def ask():
+    num = int(input("Input number({}): ".format(i + 1)))
+    return num
+
 #initialize counter
 #call function 10 times
     #check if number is odd
