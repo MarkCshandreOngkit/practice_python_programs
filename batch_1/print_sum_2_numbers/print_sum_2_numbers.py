fc299c8f7e6b3cde24bd26c05e2b604bb661ecf1
MarkCshandreOngkit <markongkit.001@gmail.com>
2025-03-11 18:23:36 +0800

    quick fix(forgot int)

--- batch_1/print_sum_2_numbers/print_sum_2_numbers.py
+++ batch_1/print_sum_2_numbers/print_sum_2_numbers.py
@@ -1,7 +1,9 @@
 #input 2 numbers
-first_number = input("Input first number: ")
-second_number = input("Input second number: ")
+first_number = int(input("Input first number: "))
+second_number = int(input("Input second number: "))
+
 #add 2 numbers
-sum = first_number + second_number 
+sum = first_number + second_number
+ 
 #print result
 print(sum)
