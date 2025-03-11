0f386a5931aa9c81c344dd0ee0466c917c2d9cbe
MarkCshandreOngkit <markongkit.001@gmail.com>
2025-03-11 18:41:06 +0800

    call function 10 times

--- batch_1/check_number_of_odds/check_number_of_odds.py
+++ batch_1/check_number_of_odds/check_number_of_odds.py
@@ -3,8 +3,12 @@ def ask():
     num = int(input("Input number({}): ".format(i + 1)))
     return num
 
-#initialize counter
+#initialize odd counter
+odd_counter = 0
+
 #call function 10 times
+for i in range(10):
+    number = ask()
     #check if number is odd
         #add 1 to odd counter if true
 #print odd counter
