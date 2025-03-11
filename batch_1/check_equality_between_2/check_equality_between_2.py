7a46b9d828706e316012c718295096d5e982e6c3
MarkCshandreOngkit <markongkit.001@gmail.com>
2025-03-11 17:25:04 +0800

    check equality and print output

--- batch_1/check_equality_between_2/check_equality_between_2.py
+++ batch_1/check_equality_between_2/check_equality_between_2.py
@@ -1,5 +1,8 @@
 #input 2 numbers
 first_number = input("Input first number: ")
 second_number = input("Input second number: ")
+
 #check if equal
+if first_number == second_number:
     #print "Equal" if true
+    print("Equal")
