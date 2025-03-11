31a411f8a365c04eb02629ad05c5bc8a36b7809e
MarkCshandreOngkit <markongkit.001@gmail.com>
2025-03-11 17:49:15 +0800

    divide and print quotient

--- batch_1/print_quotient_2_numbers_with_decimal/print_quotient_2_numbers_with_decimal.py
+++ batch_1/print_quotient_2_numbers_with_decimal/print_quotient_2_numbers_with_decimal.py
@@ -3,4 +3,7 @@ first_number = input("Input first number: ")
 second_number = input("Input second number: ")
 
 #divide 2 numbers
+quotient = first_number / second_number 
+
 #print result
+print(quotient)
