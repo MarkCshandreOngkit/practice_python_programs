11749877a10958e74a7f765eb5b19bd5a4299720
MarkCshandreOngkit <markongkit.001@gmail.com>
2025-03-11 18:16:32 +0800

    call function 10 times

--- batch_1/print_sum_10_numbers/print_sum_10_numbers.py
+++ batch_1/print_sum_10_numbers/print_sum_10_numbers.py
@@ -1,8 +1,10 @@
 #create function that ask number
 def ask():
-    num = int(input("Input number({}): ".format(i + 1)))
-    return num
+    number = int(input("Input number({}): ".format(i + 1)))
+    return number
 
 #call function 10 times
+for i in range(10):
+    number = ask()
     #add numbers each call
 #print sum
