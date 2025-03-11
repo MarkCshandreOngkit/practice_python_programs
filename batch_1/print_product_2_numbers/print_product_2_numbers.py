8d0cc5a3212650a538510243a209d2ae9838da82
MarkCshandreOngkit <markongkit.001@gmail.com>
2025-03-11 18:28:34 +0800

    quick fix(forgot int)

--- batch_1/print_product_2_numbers/print_product_2_numbers.py
+++ batch_1/print_product_2_numbers/print_product_2_numbers.py
@@ -1,6 +1,6 @@
 #input 2 numbers
-first_number = input("Input first number: ")
-second_number = input("Input second number: ")
+first_number = int(input("Input first number: "))
+second_number = int(input("Input second number: "))
 
 #multiply 2 numbers
 product = first_number * second_number 
