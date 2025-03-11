a6e787410a018e407555cda20ffab578d8b6c48f
MarkCshandreOngkit <markongkit.001@gmail.com>
2025-03-11 18:45:05 +0800

    print output

--- batch_1/check_number_of_odds/check_number_of_odds.py
+++ batch_1/check_number_of_odds/check_number_of_odds.py
@@ -10,5 +10,9 @@ odd_counter = 0
 for i in range(10):
     number = ask()
     #check if number is odd
+    if number % 2 == 1:
         #add 1 to odd counter if true
+        odd_counter += 1
+
 #print odd counter
+print(odd_counter)
