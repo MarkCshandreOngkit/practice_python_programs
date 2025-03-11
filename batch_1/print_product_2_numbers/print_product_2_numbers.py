9bb04e11f1da931ab17002a53cf24d7d4214b5fb
MarkCshandreOngkit <markongkit.001@gmail.com>
2025-03-11 17:42:01 +0800

    multiply and print product

--- batch_1/print_product_2_numbers/print_product_2_numbers.py
+++ batch_1/print_product_2_numbers/print_product_2_numbers.py
@@ -3,4 +3,7 @@ first_number = input("Input first number: ")
 second_number = input("Input second number: ")
 
 #multiply 2 numbers
+product = first_number * second_number 
+
 #print result
+print(product)
