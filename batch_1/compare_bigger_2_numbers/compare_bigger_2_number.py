3cd64bc9697b2f9ce076e2257fec84ff7308308a
MarkCshandreOngkit <markongkit.001@gmail.com>
2025-03-11 16:50:17 +0800

    input 2 numbers

---
+++ batch_1/compare_bigger_2_numbers/compare_bigger_2_number.py
@@ -0,0 +1,7 @@
+#input 2 numbers
+first_number = input("Input first number: ")
+second_number = input("Input second number: ")
+#check if first number is bigger
+    #print first number if true
+#check if second number is bigger
+    #print second number if true
