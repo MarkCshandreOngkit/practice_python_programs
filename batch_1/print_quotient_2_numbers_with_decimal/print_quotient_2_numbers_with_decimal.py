84b489b8a9a48dfdf04f5a025661cf4424fc50c0
MarkCshandreOngkit <markongkit.001@gmail.com>
2025-03-11 17:52:23 +0800

    forgot to put float after copy-pasting

--- batch_1/print_quotient_2_numbers_with_decimal/print_quotient_2_numbers_with_decimal.py
+++ batch_1/print_quotient_2_numbers_with_decimal/print_quotient_2_numbers_with_decimal.py
@@ -1,6 +1,6 @@
 #input 2 float numbers
-first_number = input("Input first number: ")
-second_number = input("Input second number: ")
+first_number = float(input("Input first number: "))
+second_number = float(input("Input second number: "))
 
 #divide 2 numbers
 quotient = first_number / second_number 
