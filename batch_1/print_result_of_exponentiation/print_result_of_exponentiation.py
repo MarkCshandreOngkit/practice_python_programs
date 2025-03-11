8cc67b275e3ac01b2e76721832fbfd8cb6f6965a
MarkCshandreOngkit <markongkit.001@gmail.com>
2025-03-11 18:25:55 +0800

    quick fix(forgot int)

--- batch_1/print_result_of_exponentiation/print_result_of_exponentiation.py
+++ batch_1/print_result_of_exponentiation/print_result_of_exponentiation.py
@@ -1,6 +1,6 @@
 #input 2 numbers
-base_number = input("Input base number: ")
-exponent = input("Input exponent: ")
+base_number = int(input("Input base number: "))
+exponent = int(input("Input exponent: "))
 
 #exponentiate 2 numbers
 power = base_number ** exponent 
