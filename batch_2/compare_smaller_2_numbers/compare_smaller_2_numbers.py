3b7536d75be7ebd693b73ecc7c174c7406e0d172
MarkCshandreOngkit <markongkit.001@gmail.com>
2025-03-13 15:01:24 +0800

    input 2 numbers

--- batch_2/compare_smaller_2_numbers/compare_smaller_2_numbers.py
+++ batch_2/compare_smaller_2_numbers/compare_smaller_2_numbers.py
@@ -1,4 +1,7 @@
 #input 2 numbers
+first_number = int(input("Input first number: "))
+second_number = int(input("Input second number: "))
+
  #check if first number is smaller
      #print first number if true
  #check if second number is smaller
