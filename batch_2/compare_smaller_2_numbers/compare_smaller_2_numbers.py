fba01910ab145116173c90a7133cd7d3ac537fec
MarkCshandreOngkit <markongkit.001@gmail.com>
2025-03-13 15:03:54 +0800

    check smaller and print output

--- batch_2/compare_smaller_2_numbers/compare_smaller_2_numbers.py
+++ batch_2/compare_smaller_2_numbers/compare_smaller_2_numbers.py
@@ -3,6 +3,11 @@ first_number = int(input("Input first number: "))
 second_number = int(input("Input second number: "))
 
  #check if first number is smaller
-     #print first number if true
- #check if second number is smaller
-     #print second number if true
+if first_number < second_number:
+    #print first number if true
+    print(first_number)
+ 
+#check if second number smaller
+elif second_number < first_number:
+    #print second number if true
+    print(second_number)
