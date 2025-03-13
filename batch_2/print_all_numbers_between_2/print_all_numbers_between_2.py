6807ad09e70a4fc0681fb6ef40663196d5ecff18
MarkCshandreOngkit <markongkit.001@gmail.com>
2025-03-13 16:27:14 +0800

    print numbers inbetween

--- batch_2/print_all_numbers_between_2/print_all_numbers_between_2.py
+++ batch_2/print_all_numbers_between_2/print_all_numbers_between_2.py
@@ -3,4 +3,6 @@ first_number = int(input("Input first number: "))
 second_number = int(input("Input second number: "))
 
 #generate numbers inbetween 2 numbers
+for number in range(first_number + 1, second_number):
     #print numbers generated
+    print(number)
