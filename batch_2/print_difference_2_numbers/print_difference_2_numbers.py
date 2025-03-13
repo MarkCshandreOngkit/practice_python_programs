3370e6c2ee0bde338be2c3e403f1102ba09c29cc
MarkCshandreOngkit <markongkit.001@gmail.com>
2025-03-13 15:17:24 +0800

    subtract and print output

--- batch_2/print_difference_2_numbers/print_difference_2_numbers.py
+++ batch_2/print_difference_2_numbers/print_difference_2_numbers.py
@@ -3,4 +3,6 @@ first_number = int(input("Input first number: "))
 second_number = int(input("Input second number: "))
 
 #subtract 2 numbers
+difference = first_number - second_number
 #print result
+print(difference)
