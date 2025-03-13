1044bc40c02e86b145ec5963d0e8ee1ceb756b40
MarkCshandreOngkit <markongkit.001@gmail.com>
2025-03-13 15:24:34 +0800

    remove decimal

--- batch_2/print_quotient_2_numbers/print_quotient_2_numbers.py
+++ batch_2/print_quotient_2_numbers/print_quotient_2_numbers.py
@@ -1,9 +1,12 @@
+#import math
+import math
+
 #input 2 numbers
 first_number = int(input("Input first number: "))
 second_number = int(input("Input second number: "))
 
-#divide 2 numbers
-quotient = first_number / second_number 
+#divide 2 numbers, add floor
+quotient = math.floor(first_number / second_number) 
 
 #print result
 print(quotient)
