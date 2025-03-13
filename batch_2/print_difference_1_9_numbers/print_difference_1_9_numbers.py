6d4c0e608cd0d2b5f62011373fea4bd3bb431001
MarkCshandreOngkit <markongkit.001@gmail.com>
2025-03-13 15:46:49 +0800

    minor fixes and print difference

--- batch_2/print_difference_1_9_numbers/print_difference_1_9_numbers.py
+++ batch_2/print_difference_1_9_numbers/print_difference_1_9_numbers.py
@@ -3,15 +3,18 @@ def ask():
     number = int(input("Input number({}): ".format(i + 1)))
     return number
 
+number = 0
+
 #call function 10 times
 for i in range(10):
     #differentiate first number
     if i == 0:
         first_number = ask()
-        print("First Number")
     else:
         number = ask()
     #subtract each numbers to first number each call
     first_number -= number
-    
+
 #print difference
+difference = first_number
+print(difference)
