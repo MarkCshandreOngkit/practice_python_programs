5a1a11ea469657d276374108a9f5b87d2a73cb0b
MarkCshandreOngkit <markongkit.001@gmail.com>
2025-03-13 15:10:56 +0800

    check inequality and print output

--- batch_2/check_inequality_between_2/check_inequality_between_2.py
+++ batch_2/check_inequality_between_2/check_inequality_between_2.py
@@ -2,5 +2,7 @@
 first_number = int(input("Input first number: "))
 second_number = int(input("Input second number: "))
 
- #check if unequal
-     #print "Not Equal" if true
+#check if unequal
+if first_number != second_number:
+    #print "Not Equal" if true
+    print("Not Equal")
