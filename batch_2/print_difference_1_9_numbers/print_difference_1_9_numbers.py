3ae8c7e5409c432f8b8933b57ceca7c9943528ad
MarkCshandreOngkit <markongkit.001@gmail.com>
2025-03-13 15:37:54 +0800

    ask number function

--- batch_2/print_difference_1_9_numbers/print_difference_1_9_numbers.py
+++ batch_2/print_difference_1_9_numbers/print_difference_1_9_numbers.py
@@ -1,4 +1,8 @@
 #create function that ask number
+def ask():
+    number = int(input("Input number({}): ".format(i + 1)))
+    return number
+
 #call function 10 times
     #differentiate first number
     #subtract each numbers to first number each call
