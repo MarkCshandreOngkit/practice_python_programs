2c0c5602169a42c13969ad1f5ed3eebae116f8c1
MarkCshandreOngkit <markongkit.001@gmail.com>
2025-03-13 15:32:52 +0800

    modulo and print output

--- batch_2/print_remainder_2_numbers/print_remainder_2_numbers.py
+++ batch_2/print_remainder_2_numbers/print_remainder_2_numbers.py
@@ -3,4 +3,7 @@ first_number = float(input("Input first number: "))
 second_number = float(input("Input second number: "))
 
 #divide 2 numbers and get remainder(modulo operator)
+remainder = first_number % second_number
+
 #print result
+print(remainder)
