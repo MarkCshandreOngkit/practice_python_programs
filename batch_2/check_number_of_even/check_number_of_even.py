acbb756303f4e68874af4b0e064d5ad491e182ac
MarkCshandreOngkit <markongkit.001@gmail.com>
2025-03-13 15:50:54 +0800

    ask number function and initialize even counter

--- batch_2/check_number_of_even/check_number_of_even.py
+++ batch_2/check_number_of_even/check_number_of_even.py
@@ -1,5 +1,11 @@
 #function that ask number
+def ask():
+    number = int(input("Input number({}): ".format(i + 1)))
+    return number
+
 #initialize even counter
+even_counter = 0
+
 #call function 10 times
     #check if number is even
         #add 1 to even counter if true
