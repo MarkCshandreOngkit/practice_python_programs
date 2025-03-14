2277529d083e20c874593a04a10f8a938904ec7b
MarkCshandreOngkit <markongkit.001@gmail.com>
2025-03-14 23:53:59 +0800

    ask function

--- batch_3/print_no_duplicate_from_10/print_no_duplicate_from_10.py
+++ batch_3/print_no_duplicate_from_10/print_no_duplicate_from_10.py
@@ -1,4 +1,8 @@
 #create ask function(input number)
+def ask():
+    number = int(input("Input number({}): ".format(i + 1)))
+    return number
+
 #initialize list
 #call function 10 times
     #check if the number is not in the list
