ce693b6a29a41a77abdd69994d5fc129e0223aac
MarkCshandreOngkit <markongkit.001@gmail.com>
2025-03-15 01:31:15 +0800

    create function and initialize

--- batch_3/sort_lowest_highest/sort_lowest_highest.py
+++ batch_3/sort_lowest_highest/sort_lowest_highest.py
@@ -1,6 +1,19 @@
 #create ask function(input number)
+def ask():
+    number = input("Input number({}): ".format(iteration))
+    return number
+
 #create function checks if input is digit/number or int
+def check_digit(number):
+    if number.isdigit():
+        return True
+    else:
+        return False
+    
 #initialize
+numbers = []
+iteration = 0
+
 #call function until invalid input(int is not inputted)(loop)
     #check if input is invalid, if true break loop
     #put the input in list
