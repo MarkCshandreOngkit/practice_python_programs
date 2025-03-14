a1745b7339a6d58a4f05b1e06114e63b16473b12
MarkCshandreOngkit <markongkit.001@gmail.com>
2025-03-15 01:32:31 +0800

    loop ask until invalid input

--- batch_3/sort_lowest_highest/sort_lowest_highest.py
+++ batch_3/sort_lowest_highest/sort_lowest_highest.py
@@ -15,7 +15,15 @@ numbers = []
 iteration = 0
 
 #call function until invalid input(int is not inputted)(loop)
+while True:
+    iteration += 1
+    number = ask()
     #check if input is invalid, if true break loop
+    check_input = check_digit(number)
+    if check_input == False:
+        break
+    int(number)
     #put the input in list
+    
 #sort list
 #print list
