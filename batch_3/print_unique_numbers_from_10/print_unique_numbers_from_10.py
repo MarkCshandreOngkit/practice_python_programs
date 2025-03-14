ce7c66dcebd83d53aa8970b1ce1fc28f97cefb00
MarkCshandreOngkit <markongkit.001@gmail.com>
2025-03-15 00:16:33 +0800

    check if unique and print output

--- batch_3/print_unique_numbers_from_10/print_unique_numbers_from_10.py
+++ batch_3/print_unique_numbers_from_10/print_unique_numbers_from_10.py
@@ -4,7 +4,16 @@ def ask():
     return number
 
 #initialize list
+unique_numbers = []
+
 #call function 10 times
+for i in range(10):
+    number = ask()
+
     #check if number is not in list
+    if number not in unique_numbers:
         #put number in list if true
+        unique_numbers.append(number)
+
+print(unique_numbers)
     
