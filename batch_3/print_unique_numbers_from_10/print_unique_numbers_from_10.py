ec3fe57d2e67ec24bbd88ed1a0218627f0ba96df
MarkCshandreOngkit <markongkit.001@gmail.com>
2025-03-15 00:13:38 +0800

    create function and initialize list

--- batch_3/print_unique_numbers_from_10/print_unique_numbers_from_10.py
+++ batch_3/print_unique_numbers_from_10/print_unique_numbers_from_10.py
@@ -1,4 +1,8 @@
 #create ask function(input number)
+def ask():
+    number = int(input("Input number({}): ".format(i + 1)))
+    return number
+
 #initialize list
 #call function 10 times
     #check if number is not in list
