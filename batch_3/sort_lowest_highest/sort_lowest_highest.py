e03103e427930b207959ba078b5b43ee9915b204
MarkCshandreOngkit <markongkit.001@gmail.com>
2025-03-15 01:30:21 +0800

    pseudocode

---
+++ batch_3/sort_lowest_highest/sort_lowest_highest.py
@@ -0,0 +1,8 @@
+#create ask function(input number)
+#create function checks if input is digit/number or int
+#initialize
+#call function until invalid input(int is not inputted)(loop)
+    #check if input is invalid, if true break loop
+    #put the input in list
+#sort list
+#print list
