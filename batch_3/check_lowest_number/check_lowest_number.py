810af3587af33bc46c60d05e927bb5b6eae57507
MarkCshandreOngkit <markongkit.001@gmail.com>
2025-03-15 01:21:19 +0800

    pseudocode

---
+++ batch_3/check_lowest_number/check_lowest_number.py
@@ -0,0 +1,9 @@
+#create ask function(input number)
+#create function checks if input is digit/number or int
+#initialize list
+#call function until invalid input(int is not inputted)(loop)
+    #check if input is invalid, if true break loop
+    #put the input in list
+#sort list
+#print first number in list
+
