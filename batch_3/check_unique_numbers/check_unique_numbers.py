6628d90c193d2d0c5701138b104f7a68f10c198e
MarkCshandreOngkit <markongkit.001@gmail.com>
2025-03-15 00:32:59 +0800

    pseudocode

---
+++ batch_3/check_unique_numbers/check_unique_numbers.py
@@ -0,0 +1,14 @@
+#create ask function(input number)
+#initialize unique and duplicate list
+#call function until invalid input(int is not inputted)
+    #check if the number is not in unique and duplicate
+        #put the number in unique if true
+    #remove if there's a duplicate and put in duplicate list
+        #check if number in unique list
+            #removes number from unique if true
+            #put the number in duplicate
+#iterate through each unique
+    #print unique number and "Unique"
+#iterate through each duplicate
+    #print duplicate number and "Duplicate"
+            
