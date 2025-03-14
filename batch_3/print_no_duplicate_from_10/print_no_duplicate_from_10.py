b18603753fcf5a232fc7d16ea7aa0f0e7ff19471
MarkCshandreOngkit <markongkit.001@gmail.com>
2025-03-15 00:07:44 +0800

    removes duplicate

--- batch_3/print_no_duplicate_from_10/print_no_duplicate_from_10.py
+++ batch_3/print_no_duplicate_from_10/print_no_duplicate_from_10.py
@@ -5,15 +5,24 @@ def ask():
 
 #initialize list
 unique_numbers = []
+duplicate = []
 
 #call function 10 times
 for i in range(10):
     number = ask()
     #check if the number is not in the list
-    if number not in unique_numbers:
+    if number not in unique_numbers and duplicate:
         #put the number in list if true
         unique_numbers.append(number)
-    #remove if there's a duplicate
+
+    #remove if there's a duplicate and put in duplicate list
+    else:
+        #checks if number in unique list
+        if number in unique_numbers:
+            #removes number from unique if true
+            unique_numbers.remove(number)
+        duplicate.append(number)
+        
 
 print(unique_numbers)
 
