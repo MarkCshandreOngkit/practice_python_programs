64c7107de23029b45c78e7166be76e1982d5dedc
MarkCshandreOngkit <markongkit.001@gmail.com>
2025-03-15 00:54:50 +0800

    checks for invalid input

--- batch_3/check_unique_numbers/check_unique_numbers.py
+++ batch_3/check_unique_numbers/check_unique_numbers.py
@@ -1,6 +1,30 @@
 #create ask function(input number)
+def ask():
+    number = input("Input number({}): ".format(iteration))
+    return number
+
+#create function checks if input is digit/number
+def check_digit(number):
+    if number.isdigit():
+        int(number)
+        return True
+    else:
+        return False
+
 #initialize unique and duplicate list
+unique_numbers = []
+duplicate = []
+iteration = 0
+
 #call function until invalid input(int is not inputted)
+while True:
+    iteration += 1
+    number = ask()
+    #check if input is valid, if true break loop
+    check_input = check_digit(number)
+    if check_input == False:
+        break
+    
     #check if the number is not in unique and duplicate
         #put the number in unique if true
     #remove if there's a duplicate and put in duplicate list
