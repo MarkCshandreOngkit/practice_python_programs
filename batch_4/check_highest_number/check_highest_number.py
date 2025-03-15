2b59468d48a815991824b58ba913c635fdba0478
MarkCshandreOngkit <markongkit.001@gmail.com>
2025-03-15 16:06:44 +0800

    pseudocode

---
+++ batch_4/check_highest_number/check_highest_number.py
@@ -0,0 +1,10 @@
+#create ask function(input number)
+#create function checks if input is digit/number
+#initialize
+#call function until invalid input(int is not inputted)
+    #check if input is invalid, if true break loop
+    #put input in numbers
+#iterate through each number
+    #check if number is greater than highest number
+        #save number as highest number
+#print highest number
