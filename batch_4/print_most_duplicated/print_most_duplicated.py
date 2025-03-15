95d72fab5b3f8f8f2d07c06b42f0ee59da0fb7f1
MarkCshandreOngkit <markongkit.001@gmail.com>
2025-03-15 15:57:10 +0800

    input till invalid input

--- batch_4/print_most_duplicated/print_most_duplicated.py
+++ batch_4/print_most_duplicated/print_most_duplicated.py
@@ -15,8 +15,17 @@ numbers = []
 iteration = 0
 
 #call function until invalid input(int is not inputted)
+while True:
+    iteration += 1
+    number = ask()
     #check if input is invalid, if true break loop
+    check_input = check_digit(number)
+    if check_input == False:
+        break
+    int(number)
     #put input in numbers
+    numbers.append(number)
+    
 #iterate through each number
     #count the times the number appeared in the list
     #check if the current count is higher than the previous count
