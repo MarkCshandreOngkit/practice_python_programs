3bf88f3d9f48d7c5b50d6caa32aa32545bc76ee7
MarkCshandreOngkit <markongkit.001@gmail.com>
2025-03-15 16:23:30 +0800

    input till invalid input

--- batch_4/print_average_from_all/print_average_from_all.py
+++ batch_4/print_average_from_all/print_average_from_all.py
@@ -15,8 +15,17 @@ numbers = []
 iteration = 0
 
 #call function until invalid input(int is not inputted)
+while True:
+    iteration += 1
+    number = ask()
     #check if input is invalid, if true break loop
+    check_input = check_digit(number)
+    if check_input == False:
+        break
+    int(number)
     #put input in numbers
+    numbers.append(number)
+    
 #iterate through each number
     #add number to sum
 #sum divided by (iteration minus 1) - because of invalid input iteration
