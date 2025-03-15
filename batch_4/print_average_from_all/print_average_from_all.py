d2b4fd7373ca3fe19dfe8156cae3b0cb7853cb8b
MarkCshandreOngkit <markongkit.001@gmail.com>
2025-03-15 16:21:55 +0800

    pseudocode

---
+++ batch_4/print_average_from_all/print_average_from_all.py
@@ -0,0 +1,9 @@
+#create ask function(input number)
+#create function checks if input is digit/number
+#initialize
+#call function until invalid input(int is not inputted)
+    #check if input is invalid, if true break loop
+    #put input in numbers
+#iterate through each number
+    #add number to sum
+#sum divided by (iteration minus 1) - because of invalid input iteration
