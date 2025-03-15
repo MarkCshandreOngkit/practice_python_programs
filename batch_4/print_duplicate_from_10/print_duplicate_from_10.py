0024db1cf3088275b8b1fee1e06e6e3b04fcb248
MarkCshandreOngkit <markongkit.001@gmail.com>
2025-03-15 14:37:01 +0800

    create function, initialize, and call function 10 times

--- batch_4/print_duplicate_from_10/print_duplicate_from_10.py
+++ batch_4/print_duplicate_from_10/print_duplicate_from_10.py
@@ -1,6 +1,16 @@
 #create ask function(input number)
+def ask():
+    number = int(input("Input number({}): ".format(i + 1)))
+    return number
+
 #initialize lists
+unique_numbers = []
+duplicates = []
+
 #call function 10 times
+for i in range(10):
+    number = ask()
+
     #check if the number is not in unique and duplicate
         #put the number in unique if true
     #remove in unique if there's a duplicate and put in duplicate list
