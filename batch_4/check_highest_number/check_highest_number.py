56f98ef884189e6b6db90efc2326a529bab411ee
MarkCshandreOngkit <markongkit.001@gmail.com>
2025-03-15 16:09:24 +0800

    input till invalid input

--- batch_4/check_highest_number/check_highest_number.py
+++ batch_4/check_highest_number/check_highest_number.py
@@ -16,8 +16,17 @@ iteration = 0
 highest_number = 0
 
 #call function until invalid input(int is not inputted)
+while True:
+    iteration += 1
+    number = ask()
     #check if input is invalid, if true break loop
+    check_input = check_digit(number)
+    if check_input == False:
+        break
+    int(number)
     #put input in numbers
+    numbers.append(number)
+
 #iterate through each number
     #check if number is greater than highest number
         #save number as highest number
