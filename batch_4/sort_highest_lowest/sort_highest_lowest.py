dddc09461fc77010d5854df3b5de8f96b2938777
MarkCshandreOngkit <markongkit.001@gmail.com>
2025-03-15 16:39:34 +0800

    quick fix

--- batch_4/sort_highest_lowest/sort_highest_lowest.py
+++ batch_4/sort_highest_lowest/sort_highest_lowest.py
@@ -22,7 +22,7 @@ while True:
     check_input = check_digit(number)
     if check_input == False:
         break
-    int(number)
+    number = int(number)
     #put input in numbers
     numbers.append(number)
 
