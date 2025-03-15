c9f048256f3d71e8ef521e62d245a249d3841c5b
MarkCshandreOngkit <markongkit.001@gmail.com>
2025-03-15 14:40:21 +0800

    separate unique and duplicate, print output

--- batch_4/print_duplicate_from_10/print_duplicate_from_10.py
+++ batch_4/print_duplicate_from_10/print_duplicate_from_10.py
@@ -12,8 +12,17 @@ for i in range(10):
     number = ask()
 
     #check if the number is not in unique and duplicate
+    if number not in unique_numbers and number not in duplicates:
         #put the number in unique if true
+        unique_numbers.append(number)
+
     #remove in unique if there's a duplicate and put in duplicate list
+    else:
         #checks if number in unique list
+        if number in unique_numbers:
             #removes number from unique if true
+            unique_numbers.remove(number)
+        duplicates.append(number)
+
 #print duplicate
+print(duplicates)
