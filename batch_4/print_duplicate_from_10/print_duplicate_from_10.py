cbc08341caa901cee7d6d7903da8ef2d9e3c8b67
MarkCshandreOngkit <markongkit.001@gmail.com>
2025-03-15 14:34:48 +0800

    pseudocode

---
+++ batch_4/print_duplicate_from_10/print_duplicate_from_10.py
@@ -0,0 +1,9 @@
+#create ask function(input number)
+#initialize lists
+#call function 10 times
+    #check if the number is not in unique and duplicate
+        #put the number in unique if true
+    #remove in unique if there's a duplicate and put in duplicate list
+        #checks if number in unique list
+            #removes number from unique if true
+#print duplicate
