dcce044c1ae496ab4db0d99e9beb5d924dabb503
MarkCshandreOngkit <markongkit.001@gmail.com>
2025-03-15 15:52:45 +0800

    pseudocode

---
+++ batch_4/print_most_duplicated/print_most_duplicated.py
@@ -0,0 +1,13 @@
+#create ask function(input number)
+#create function checks if input is digit/number
+#initialize
+#call function until invalid input(int is not inputted)
+    #check if input is invalid, if true break loop
+    #put input in numbers
+#iterate through each number
+    #count the times the number appeared in the list
+    #check if the current count is higher than the previous count
+        #save the number as most duplicate
+#print most duplicate
+
+
